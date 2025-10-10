dea783f70bb37175ca42ac92dbf5359fee7138e8
Tabeha <makar.egorov.04@mail.ru>
2025-10-10 11:24:08 +0300

    lol4k

--- api/__main__.py
+++ api/__main__.py
@@ -1,6 +1,8 @@
 from .config import config
 from .v1.routes import attractiveness_router
 from .models import attractiveness_model
+from .v1.users import UserRouter
+# from fastapi.middleware.cors import CORSMiddleware
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
@@ -18,7 +20,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+# do we need to add CORSMiddleware?
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=False,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.include_router(attractiveness_router, prefix="/attractiveness")
+# app.include_router(UserRouter, tags=["User"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host=config.api_host, port=config.api_port)
